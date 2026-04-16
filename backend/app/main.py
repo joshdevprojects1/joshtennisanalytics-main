94c50b41e5f891ae1c768d58bb4f44f832b3dc4e
joshdevprojects1 <joshdevprojects1@gmail.com>
2026-04-16 17:18:08 -0500

    add railway to origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://joshtennisanalytics-josh-hilgers-projects.vercel.app/"
+        "https://joshtennisanalytics-josh-hilgers-projects.vercel.app/",
+        "https://joshtennisanalytics-production.up.railway.app"
         # Add your production frontend URL here
     ],
     allow_credentials=True,
