2f51392e8e3fd09df00e8b905e2fdd59c1e0082b
Your Name <you@example.com>
2026-04-16 16:58:28 -0500

    add vercel to origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,6 +38,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://joshtennisanalytics-josh-hilgers-projects.vercel.app/"
         # Add your production frontend URL here
     ],
     allow_credentials=True,
